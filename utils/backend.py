ccde47dc2d18745b8b24232396eb9120b11db9e2
jdfr <jdfr@users.noreply.github.com>
2021-06-28 16:03:31 +0200

    fix bug introduced with last changes to this file

--- utils/backend.py
+++ utils/backend.py
@@ -4,7 +4,7 @@ import json
 import warnings
 import time
 
-fb = firebase.FirebaseApplication("https://smart-campus-uma.firebaseio.com/", None)
+#fb = firebase.FirebaseApplication("https://smart-campus-uma.firebaseio.com/", None)
 
 
 def post_minute_data(data, fb):
@@ -13,15 +13,16 @@ def post_minute_data(data, fb):
         # Get timing
         now = datetime.datetime.now()
         day = now.date()
-        hour_min = now.strftime("%H:%M")
+        hour_min = now.strftime("%Y:%m:%d:%H:%M")  # "%H:%M")
         print(hour_min)
         # Convert Data Dict to JSON
-        data_json = json.dumps(data)
+        #data_json = json.dumps(data)
         # Set variable dinamically
         url = str('/streetqr/counter/' + str(day) + '/')
         # Post Data
         try:
-                fb.patch(url, {hour_min: data_json})
+                doc_ref = fb.collection('minute_data').document(hour_min)
+                doc_ref.set(data) #data_json)
         except:
                 warnings.warn('Failed to POST data')
 
@@ -33,11 +34,15 @@ def post_daily_data(data, fb):
         # yesterday = datetime.datetime.now().date() - datetime.timedelta(1)
         # Set variable dinamically
         url = str('/streetqr/counter/total/')
-        data_json = json.dumps(data)
+        #data_json = json.dumps(data)
+
+        now = datetime.datetime.now()
+        day = now.date()
 
         # Post Data
         try:
-                fb.patch(url, {'total': data_json})
+                doc_ref = fb.collection('daily_data').document(day)
+                doc_ref.set(data) #data_json)
 
         except:
                 print('Failed to POST daily data')
